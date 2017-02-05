40ad133b2d046e33d97d10d5b737f7c93faf9604
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-05 00:36:30 -0500

    trying to use all ES6 imports

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,6 @@
-// const sum = require('./sum')
 import sum from './sum'
-const snapshot = require('snap-shot')
+import snapshot from 'snap-shot'
+// const snapshot = require('snap-shot')
 
 it('2 + 5 should match snap-shot', () => {
   const result = sum(2, 5)
