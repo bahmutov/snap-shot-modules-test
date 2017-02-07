1ab7bc674fdc1523baebb55b2ac228b5f468f918
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-02-07 00:28:23 -0500

    multiple values test

--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,13 @@
 import sum from './sum'
 import snapshot from 'snap-shot'
-// const snapshot = require('snap-shot')
 
 it('2 + 5 should match snap-shot', () => {
   const result = sum(2, 5)
   snapshot(result)
+})
+
+it('multiple values', () => {
+  snapshot('first')
+  snapshot('second')
+  snapshot('third')
 })
